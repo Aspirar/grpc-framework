44a180b1d0a5eb2461b54e8b50dd8f4a619ddc42
Lakshya Gupta <lakshya@thesilverlabs.com>
2020-08-19 15:53:55 +0530

    Add files

---
+++ example/client.js
@@ -0,0 +1,44 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const path = require('path');
+
+const protoFile = path.join(__dirname, 'example.proto');
+
+const packageDefinition = protoLoader.loadSync(protoFile, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+const exampleProto = grpc.loadPackageDefinition(packageDefinition).example;
+
+async function run() {
+  const client = new exampleProto.Example('localhost:50051', grpc.credentials.createInsecure());
+  const user = process.argv.length >= 3 ? process.argv[2] : 'World';
+  client.hello({ name: user }, (err, response) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Greeting:', response.message);
+  });
+
+  client.helloTwo({ name: user }, (err, response) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Greeting:', response.message);
+  });
+
+  client.helloThree({ name: user }, (err, response) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Greeting:', response.message);
+  });
+}
+
+run();
